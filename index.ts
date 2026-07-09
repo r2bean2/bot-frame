e93b3d7a0a6f9cb24e8de90cda6892677783e267
Calivn <Calvin>
2026-07-09 12:55:10 -0400

    New erroer finder

---
+++ index.ts
@@ -0,0 +1,8 @@
+import worker_threads from "node:worker_threads"
+function runScript(pathname:string){
+    const worker=new worker_threads.Worker(pathname)
+    worker.once("error",(error)=>{
+        console.error(error)
+    })
+}
+runScript("./text_bots_worker.ts")
